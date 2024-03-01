23ff9e255f2609abfae9b5fc2e091f910f19015f
Matthew Wildoer <mawildoer@gmail.com>
2024-03-01 11:22:23 -0800

    Get the popped IMU running

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "BMI088.h"
-#include "Adafruit_BNO055.h"
 #include "LIS3MDL.h"
+#include "SensorFusion.h"
 
 LIS3MDL mag;
+Bmi088 bmi(Wire, 0x19, 0x69);
+SF fusion;
+float gx, gy, gz, ax, ay, az, mx, my, mz;
+float pitch, roll, yaw;
+float deltat;
 
 char report[80];
 
@@ -25,24 +30,64 @@ void setup()
 
 	if (!mag.init())
 	{
-		while (1) {
+		while (1)
+		{
 			Serial.println("Failed to detect and initialize magnetometer!");
 			delay(1000);
 		}
 	}
-	// mag.enableDefault();
-	// Serial.println("Magnetometer started!");
+	mag.enableDefault();
+	Serial.println("Magnetometer started!");
+
+	// Start the BNO055 sensor
+	if (!bmi.begin())
+	{
+		while (1) {
+			Serial.print("No BMI088 detected");
+			delay(1000);
+		}
+	}
+	Serial.println("BMI088 started!");
 }
 
 void loop()
 {
-	// mag.read();
+	mag.read();
+	bmi.readSensor();
 
-	// snprintf(
-	// 	report, sizeof(report), "M: %6d %6d %6d",
-	// 	mag.m.x, mag.m.y, mag.m.z);
-	// Serial.println(report);
-	Serial.println("Hello World!");
+	snprintf(
+		report, sizeof(report), "M: %6d %6d %6d",
+		mag.m.x, mag.m.y, mag.m.z);
+	Serial.println(report);
+	mx = mag.m.x;
+	my = mag.m.y;
+	mz = mag.m.z;
+
+	snprintf(
+		report, sizeof(report), "T: %6.2f",
+		bmi.getTemperature_C()
+	);
+	Serial.println(report);
+	// ax = bmi.getAccelX_mss();
+	// ay = bmi.getAccelY_mss();
+	// az = bmi.getAccelZ_mss();
+	// gx = bmi.getGyroX_rads();
+	// gy = bmi.getGyroY_rads();
+	// gz = bmi.getGyroZ_rads();
+
+	// deltat = fusion.deltatUpdate(); //this have to be done before calling the fusion update
+	// //choose only one of these two:
+	// // fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat);  //mahony is suggested if there isn't the mag and the mcu is slow
+	// fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);  //else use the magwick, it is slower but more accurate
+
+	// pitch = fusion.getPitch();
+	// roll = fusion.getRoll();    //you could also use getRollRadians() ecc
+	// yaw = fusion.getYaw();
+
+	// Serial.print("Pitch:\t"); Serial.println(pitch);
+	// Serial.print("Roll:\t"); Serial.println(roll);
+	// Serial.print("Yaw:\t"); Serial.println(yaw);
+	// Serial.println();
 
 	delay(100);
 }
