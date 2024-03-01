4ac270d4e7a2d81cfbbd6f2783ab0983fc6dd9e3
Matthew Wildoer <mawildoer@gmail.com>
2024-03-01 15:41:37 -0800

    Theoretically working 2-chip solution

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -5,11 +5,13 @@
 #include "SensorFusion.h"
 
 LIS3MDL mag;
-Bmi088 bmi(Wire, 0x19, 0x69);
+Bmi088 bmi(Wire, 0x18, 0x68);
+
 SF fusion;
 float gx, gy, gz, ax, ay, az, mx, my, mz;
 float pitch, roll, yaw;
 float deltat;
+float T;
 
 char report[80];
 
@@ -40,21 +42,26 @@ void setup()
 	Serial.println("Magnetometer started!");
 
 	// Start the BNO055 sensor
-	if (!bmi.begin())
+	delay(1000);
+	int bmi_status = bmi.begin();
+	if (bmi_status < 1)
 	{
-		while (1) {
-			Serial.print("No BMI088 detected");
+		while (1)
+		{
+			snprintf(report, sizeof(report), "Failed to init BMI088! Status: %d", bmi_status);
+			Serial.println(report);
 			delay(1000);
 		}
 	}
 	Serial.println("BMI088 started!");
+
+	delay(1000);
+	Serial.println("Starting...");
 }
 
 void loop()
 {
 	mag.read();
-	bmi.readSensor();
-
 	snprintf(
 		report, sizeof(report), "M: %6d %6d %6d",
 		mag.m.x, mag.m.y, mag.m.z);
@@ -63,31 +70,33 @@ void loop()
 	my = mag.m.y;
 	mz = mag.m.z;
 
+	bmi.readSensor();
+	T = bmi.getTemperature_C();
+	ax = bmi.getAccelX_mss();
+	ay = bmi.getAccelY_mss();
+	az = bmi.getAccelZ_mss();
+	gx = bmi.getGyroX_rads();
+	gy = bmi.getGyroY_rads();
+	gz = bmi.getGyroZ_rads();
+
 	snprintf(
-		report, sizeof(report), "T: %6.2f",
-		bmi.getTemperature_C()
-	);
+		report, sizeof(report), "T: %6.2f A: %6.2f %6.2f %6.2f G: %6.4f %6.4f %6.4f",
+		T, ax, ay, az, gx, gy, gz);
 	Serial.println(report);
-	// ax = bmi.getAccelX_mss();
-	// ay = bmi.getAccelY_mss();
-	// az = bmi.getAccelZ_mss();
-	// gx = bmi.getGyroX_rads();
-	// gy = bmi.getGyroY_rads();
-	// gz = bmi.getGyroZ_rads();
 
-	// deltat = fusion.deltatUpdate(); //this have to be done before calling the fusion update
-	// //choose only one of these two:
-	// // fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat);  //mahony is suggested if there isn't the mag and the mcu is slow
-	// fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);  //else use the magwick, it is slower but more accurate
+	deltat = fusion.deltatUpdate(); //this have to be done before calling the fusion update
+	//choose only one of these two:
+	// fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat);  //mahony is suggested if there isn't the mag and the mcu is slow
+	fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);  //else use the magwick, it is slower but more accurate
 
-	// pitch = fusion.getPitch();
-	// roll = fusion.getRoll();    //you could also use getRollRadians() ecc
-	// yaw = fusion.getYaw();
+	pitch = fusion.getPitch();
+	roll = fusion.getRoll();    //you could also use getRollRadians() ecc
+	yaw = fusion.getYaw();
 
-	// Serial.print("Pitch:\t"); Serial.println(pitch);
-	// Serial.print("Roll:\t"); Serial.println(roll);
-	// Serial.print("Yaw:\t"); Serial.println(yaw);
-	// Serial.println();
+	Serial.print("Pitch:\t"); Serial.println(pitch);
+	Serial.print("Roll:\t"); Serial.println(roll);
+	Serial.print("Yaw:\t"); Serial.println(yaw);
+	Serial.println();
 
 	delay(100);
 }
