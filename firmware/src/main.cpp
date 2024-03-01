d56aad28d710b5ca5c59f9d41fd03ae445182814
Matthew Wildoer <mawildoer@gmail.com>
2024-03-01 10:46:25 -0800

    Deps installed and hello world running

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,13 +1,48 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "BMI088.h"
+#include "Adafruit_BNO055.h"
+#include "LIS3MDL.h"
 
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+LIS3MDL mag;
+
+char report[80];
+
+void setup()
+{
+	Serial.begin(115200);
+	while (!Serial)
+		;
+	Serial.println("Serial started!");
+
+	// Configure the i2c bus (Wire) to use:
+	//  - i2c.sda ~ gpio20
+	//  - i2c.scl ~ gpio21
+	Wire.setSDA(20);
+	Wire.setSCL(21);
+	Wire.begin();
+	Serial.println("I2C started!");
+
+	if (!mag.init())
+	{
+		while (1) {
+			Serial.println("Failed to detect and initialize magnetometer!");
+			delay(1000);
+		}
+	}
+	// mag.enableDefault();
+	// Serial.println("Magnetometer started!");
 }
 
-void loop() {
-  // print out a message
-  Serial.println("Hello, world!");
-  // wait for a second
-  delay(1000);
+void loop()
+{
+	// mag.read();
+
+	// snprintf(
+	// 	report, sizeof(report), "M: %6d %6d %6d",
+	// 	mag.m.x, mag.m.y, mag.m.z);
+	// Serial.println(report);
+	Serial.println("Hello World!");
+
+	delay(100);
 }
